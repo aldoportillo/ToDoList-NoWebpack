c6f7dfb90d348cce0bd60193abaf3b4b27457f3c
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-23 12:10:17 -0500

    Make form submit information to the console

--- main.js
+++ main.js
@@ -9,9 +9,19 @@ const createToDo = (Title, Description, DueDate, Priority, CheckList) => {
 }
 
 
+
 createToDo("Homework", "To do list", "08/20/22", "low", "false");
 
 createToDo("Work", "Bartend", "Today", "Urgent", "false");
 
 createToDo("Train", "MMA", "Tomorrow", "Top Priority", "false")
  
+const form = document.querySelector("form");
+const submitFormButton = document.getElementById('s');
+
+submitFormButton.addEventListener('click', () => {
+    createToDo(document.querySelector("#title").value, document.querySelector('#description').value, document.querySelector('#dueDate').value, document.querySelector('#priority').value, document.querySelector('#checklist').value )
+
+    form.reset();
+})
+

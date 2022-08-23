0babcfeaa0cf92d9b7e2abc703c8d00f0f5a611f
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-23 15:22:42 -0500

    Update DOM using event handler with values input to the form

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 let toDoArr = [];
 
+const titleContent = document.getElementById('title-h2');
+const descriptionContent = document.getElementById('description-p');
+const dueDateContent = document.getElementById('due-date-p');
+const priorityContent = document.getElementById('priority-p');
+const checkListContent = document.getElementById('checklist-p')
+
 const createToDo = (Title, Description, DueDate, Priority, CheckList) => {
     
     toDoArr.push({Title, Description, DueDate, Priority, CheckList})
     
     console.table(toDoArr);
-    return {Title, Description, DueDate, Priority, CheckList} // Maybe just return object for DOM Manipulation or return the Arr
+    return toDoArr // Maybe just return object for DOM Manipulation or return the Arr
 }
 
 
@@ -19,8 +25,15 @@ createToDo("Train", "MMA", "Tomorrow", "Top Priority", "false")
 const form = document.querySelector("form");
 const submitFormButton = document.getElementById('s');
 
+
 submitFormButton.addEventListener('click', () => {
     createToDo(document.querySelector("#title").value, document.querySelector('#description').value, document.querySelector('#dueDate').value, document.querySelector('#priority').value, document.querySelector('#checklist').value )
+
+    titleContent.textContent = toDoArr[toDoArr.length - 1].Title
+    descriptionContent.textContent = toDoArr[toDoArr.length - 1].Description;
+    dueDateContent.textContent = toDoArr[toDoArr.length - 1].DueDate;
+    priorityContent.textContent = toDoArr[toDoArr.length - 1].Priority;
+    checkListContent.textContent = toDoArr[toDoArr.length - 1].CheckList;
 
     form.reset();
 })

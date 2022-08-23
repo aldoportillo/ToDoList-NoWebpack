fe8495c2560182a6b2c69b9d445f7bcf43300bf2
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-08-23 11:55:53 -0500

    Create a console table for testing

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+let toDoArr = [];
+
+const createToDo = (Title, Description, DueDate, Priority, CheckList) => {
+    
+    toDoArr.push({Title, Description, DueDate, Priority, CheckList})
+    
+    console.table(toDoArr);
+    return {Title, Description, DueDate, Priority, CheckList} // Maybe just return object for DOM Manipulation or return the Arr
+}
+
+
+createToDo("Homework", "To do list", "08/20/22", "low", "false");
+
+createToDo("Work", "Bartend", "Today", "Urgent", "false");
+
+createToDo("Train", "MMA", "Tomorrow", "Top Priority", "false")
+ 
